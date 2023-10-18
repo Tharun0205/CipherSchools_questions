7d6af371643dac539c9ef99739712341c5c5db82
Tarun <117061657+Tharun0205@users.noreply.github.com>
2023-10-18 16:57:18 +0530

    Add files via upload

---
+++ lecture9.2.0.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int sum(int a,int b);
+int main(){
+	int a,b;
+	a=3;
+	b=5;
+	int c;
+	cout<<a<<" "<<b<<endl;
+	c=sum(a,b);
+	cout<<c;
+	cout<<a<<" "<<b<<endl;
+	return 0;
+}
+int sum(int a,int b){
+	return a+b;
+}
